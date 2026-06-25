40648fc54d8e00b201919d2d09695e49be0bee2a
Prakalya G R <grprakalya081107@gmail.com>
2026-06-25 17:59:41 +0530

    Add files via upload

---
+++ jsfiles/script.js
@@ -0,0 +1,110 @@
+const toggle=document.querySelector(".nav-toggle");
+const menu=document.querySelector(".nav-menu");
+const close=document.querySelector(".menu-close");
+
+toggle.addEventListener("click", ()=>{
+    menu.classList.add("active");
+    toggle.style.display="none";
+});
+
+close.addEventListener("click", ()=>{
+    menu.classList.remove("active");
+    toggle.style.display="flex";
+});
+
+
+const destinations = [
+  "Chennai",
+  "Hyderabad",
+  "Mumbai",
+  "Goa",
+  "Jaipur",
+  "Udaipur",
+  "Manali",
+  "Shimla",
+  "Pondicherry",
+  "Darjeeling",
+  "Ooty",
+  "Coorg"
+];
+
+const input=document.getElementById("destsearch");
+const suggestions=document.getElementById("suggestions");
+
+input.addEventListener("input", ()=>{
+    const value=input.value.toLowerCase();
+    suggestions.innerHTML="";
+
+    if(value==="") return;
+
+    const matches=destinations.filter(dest=>{
+        return dest.toLowerCase().startsWith(value);
+    });
+
+    matches.forEach(dest=>{
+        const div=document.createElement("div");
+        div.classList.add("suggestion-item");
+        div.textContent=dest;
+
+        div.addEventListener("click", ()=>{
+            input.value=dest;
+            suggestions.innerHTML=" ";
+        });
+        suggestions.appendChild(div);
+    });
+
+});
+
+
+
+const checkin = document.getElementById("checkin");
+const checkout = document.getElementById("checkout");
+const today = new Date().toISOString().split("T")[0];
+
+searchroomsbtn.addEventListener("click",()=>{
+    if (checkin.value && checkin.value < today) {
+        alert("Check-in date cannot be in the past");
+    }
+    else if (checkout.value && checkout.value< checkin.value) {
+        alert("Check-out date must be after check-in date");
+    }
+    //TODO: connect search to hotel filtering
+});
+
+
+//viewmoredestinations
+
+const cards = document.querySelectorAll(".destcard");
+
+function updateWhenResize(){
+    const visibleCards = window.innerWidth <= 768 ? 4 : 6;
+cards.forEach((card, index)=>{
+    card.style.display = index < visibleCards ? "" : "none";
+
+});
+}
+
+const moredestbtn = document.getElementById("moredest");
+
+let expanded = false;
+
+moredestbtn.addEventListener("click", () => {
+    if (!expanded) {
+        cards.forEach(card => {
+            card.style.display = "block";
+        });
+
+        moredestbtn.textContent = "Show Less ^";
+        expanded = true;
+    } else {
+        updateWhenResize();
+        moredestbtn.textContent = "More Destinations >";
+        expanded = false;
+    }
+});
+
+updateWhenResize();
+
+window.addEventListener("resize", ()=>{
+    if(!expanded) updateWhenResize()});
+
