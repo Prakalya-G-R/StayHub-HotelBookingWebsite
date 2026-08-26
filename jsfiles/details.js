caafe0d34f6c94b1b33726b8fe8fbb5a4b077882
Prakalya G R <grprakalya081107@gmail.com>
2026-08-26 21:32:23 +0530

    booking card styling

--- jsfiles/details.js
+++ jsfiles/details.js
@@ -27,6 +27,7 @@ if(hotel){
     const checkout=document.getElementById("checkout");
     const guests = document.getElementById("guestCount");
     const priceper=document.getElementById("price-per-night");
+    const amenities=document.getElementsByClassName(".amenities-list");
     
     hotelTitle.innerHTML=`
     <h1>${hotel.name}</h1>
@@ -36,6 +37,7 @@ if(hotel){
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-star-icon lucide-star"><path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"/></svg>
     </div></div>`;
 
+
     mainImg.innerHTML=`
     <img src="${hotel.images.cover}" alt="${hotel.name}">`;
 
@@ -44,9 +46,13 @@ if(hotel){
     <img src="${hotel.images.pool}" alt="pool">
     <img src="${hotel.images.restaurant}" alt="restaurant">`;
 
-    priceper.textContent=`${hotel.price}`;
+    amenitiesList.innerHTML = hotel.amenities
+    .map(item => `<div class="amenity">${item}</div>`)
+    .join("");
 
-    priceval.textContent=`${hotel.price.toLocaleString()}`;
+    priceper.textContent=`${hotel.price.toLocaleString("en-IN")}`;
+
+    priceval.textContent=`${hotel.price.toLocaleString("en-IN")}`;
 
     guests.innerHTML=`${hotel.room.guests}`;
 
@@ -56,6 +62,7 @@ if(hotel){
     const roomsInput = document.getElementById("rooms");
     const guestCount = document.getElementById("guestCount");
     const finalPrice = document.getElementById("price-val");
+    const rupee = document.getElementById("rupee");
 
     function updateGuests() {
         const total = roomCapacity * Number(roomsInput.value);
@@ -72,8 +79,12 @@ if(hotel){
         const nights=getNights();
         if (nights === 0) {
         finalPrice.textContent = "Select dates";
+        finalPrice.style.color="rgb(153, 153, 251)"
+        rupee.style.display="none";
         return;
         }
+        rupee.style.display="block";
+        finalPrice.style.color="blue";
         const total = totalPrice * nights * Number(roomsInput.value);
         finalPrice.textContent=`${total.toLocaleString()}`;
     }
